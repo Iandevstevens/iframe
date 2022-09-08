cc179669f1530ef48295d40971f1156182951659
Ian Stevens <iandevstevens@gmail.com>
2022-09-08 14:05:57 +0200

    testing

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ IP: req.headers["x-forwarded-for"]?.split(",").shift() || req.socket?.remoteAddress });
+  res.status(200).json({ IP: req.headers["x-forwarded-for"]?.split(",").shift() || req.socket?.remoteAddress, IP2: req.socket.localAddress });
 }
