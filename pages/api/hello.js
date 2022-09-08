d27c292fb80d1d8f69cce9da12beee65618afbf3
Ian Stevens <iandevstevens@gmail.com>
2022-09-08 13:57:27 +0200

    return IP adress

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ IP: req.headers["x-forwarded-for"]?.split(",").shift() || req.socket?.remoteAddress });
 }
